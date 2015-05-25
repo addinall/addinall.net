131f15811ebe89bd95cac27e11534881d7f88a6b
Addinall <addinall@addinall.net>
2015-05-26 02:17:42 +1000

    Added Mongo DB connectivity

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 // vim: set expandtab tabstop=4 shiftwidth=4 autoindent smartindent: -->
+
+Parties = new Mongo.Collection("parties");
+
 if (Meteor.isClient) {
     angular.module('socially', ['angular-meteor']);
 
-    angular.module("socially").controller("PartiesListCtrl", ['$scope',
-        function($scope) {
+    angular.module("socially").controller("PartiesListCtrl", ['$scope', '$meteor',
+        function($scope, $meteor) {
+            $scope.parties = $meteor.collection(Parties);
             $scope.name = "Wurld";
-            $scope.parties = [
+        }]);
+    }
+
+
+
+if (Meteor.isServer) {
+    Meteor.startup(function() {
+        if (Parties.find().count() === 0) {
+            var parties = [
                 {'name':    'Dubstep-Free Zone',
                     'description':  'Can we please for an evening not listen to dubstep.'},
                 {'name':    'All dubstep all the time',
@@ -13,8 +25,10 @@ if (Meteor.isClient) {
                 {'name':    'Savage lounging',
                     'description':  'Leisure suit required. And only fiercest manners.'}
             ];
-        }]);
-    }
 
-
-
+            for (var i = 0; i < parties.length; i++) {
+                Parties.insert(parties[i]);
+            }
+        }
+    });
+}
