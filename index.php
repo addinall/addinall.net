6f8263462f841584af2e1ffc86e35ff745eb06f3
Addinall <addinall@addinall.net>
2015-06-13 20:22:59 +1000

    Sass changes

--- index.php
+++ index.php
@@ -17,26 +17,41 @@
 
 <html>
 <head>
+<title>addinall.net - demonstration of new architectures</title>
+
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css">
+<meta name="author" content="Mark Addinall - Brisbane Australia">
+<meta name="keywords" content="HTML5 CSS3 WebGL AJAX REST jQuery MOBILE">
+
+
 <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
 <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+
+<link href='css/style.css' rel='stylesheet' type='text/css'>
+
 </head>
 <body>
 
-<div data-role="page">
-  <div data-role="header">
-    <h1>Welcome To My Homepage</h1>
-  </div>
-
-  <div data-role="main" class="ui-content">
-    <p>I Am Now A Mobile Developer!!</p>
-  </div>
-
-  <div data-role="footer">
-    <h1>Footer Text</h1>
-  </div>
-</div> 
+<div id='stars'></div>
+<div id='stars2'></div>
+<div id='stars3'></div>
+<div id='title'>
+  <span>
+     addinall.net 
+  </span>
+  <br>
+  <span>
+      A DEMONSTRATION 
+  </span>
+  <br>
+  <span>
+     HTML5/SCSS/jQuery-MOBILE
+  </span>
+  <br>
+  <span>
+     WebGL/REST-AJaX
+  </span>
+</div>
 
 </body>
 </html>
